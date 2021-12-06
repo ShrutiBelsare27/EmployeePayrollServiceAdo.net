f70f3ac657a4cda44a6db12e999571c50b12d996
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-12-06 18:43:33 +0530

    Added: Establishing connection

---
+++ EmployeePayrollServiceAdo.net/EmployeePayrollServiceAdo.net/EmployeeRepo.cs
@@ -0,0 +1,28 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollServiceAdo.net
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = "Server=localhost;Database=student; Uid=root;Pwd=root;";
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        public void checkConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                Console.WriteLine("connection established");
+                this.connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+    }
+}
---
+++ EmployeePayrollServiceAdo.net/EmployeePayrollServiceAdo.net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeePayrollServiceAdo.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcomr to the Employee payroll service ado .net program");
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            employeeRepo.checkConnection();
+           
+        }
+    }
+}
